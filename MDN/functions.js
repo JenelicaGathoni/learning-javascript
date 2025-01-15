50cdbf0a2ac6707a2cb91d94d41141590f8e6bc4
JenelicaGathoni <168233892+JenelicaGathoni@users.noreply.github.com>
2025-01-15 19:57:26 +0300

    learnt about arrow functions and this keyword

--- MDN/functions.js
+++ MDN/functions.js
@@ -94,3 +94,86 @@ const pet = function (name) {
 };
 const myPet = pet("Vivie");
 console.log(myPet());
+//Multiply nested functions
+function A(x) {
+  function B(y) {
+    function C(z) {
+      console.log(x + y + z);
+    }
+    C(3);
+  }
+  B(2);
+}
+A(1);
+//Function parameters
+// Default parameters allow named parameters to be initialized with default values if no value or undefined is passed
+function multiply(a, b = 1) {
+  return a * b;
+}
+console.log(multiply(5));
+// Rest Parameters
+// allows a function to accept an indefinite number of arguments as an arrray providing a way to represent variadic funtions
+// a funtion can only have one rest parameter
+// the rest parameter must be the last parameter in the function definition
+// the rest parameter cannot have a default value
+function sum(...theArgs) {
+  let total = 0;
+  for (const arg of theArgs) {
+    total += arg;
+  }
+  return total;
+}
+console.log(sum(5, 6, 7));
+
+// This Keyword
+//Refers to the  context where a piece of code such as a function's body is supposed to run
+function sayHi() {
+  // this in this case refers to the global environment
+  console.log(this);
+}
+// inside the funtion this means the student object
+sayHi();
+const student = {
+  name: "Jenelle",
+  sayName: function () {
+    console.log(this.name);
+  },
+};
+student.sayName();
+
+// In a class or constructor function
+class Robot {
+  constructor(name) {
+    this.name = name;
+  }
+  sayHello() {
+    console.log(`Hello, my name is ${this.name}`);
+  }
+}
+const robot1 = new Robot("Cayyene");
+robot1.sayHello();
+
+// Arrow functions/ fat arrow
+// An arrow function expression is a compact alternative to a
+//  traditional function expression with some semantic differences
+//  and deliberate limitaitions in usage
+
+// theu don't have their own bindings to this arguments or super and should not be used as methods
+// they cannot be used as constructors (callin them with new throws a typeError)
+// They cannot use yield within their body and cannot be created as generator functions
+
+// Destructuring traditional functional expressions to form an arrow function
+(function (a) {
+  return a + 100;
+});
+// 1. Remove the word function and place arrow btwn argument and opening body brace
+(a) => {
+  return a + 100;
+};
+// 2.Remove the body braces and word return
+// the braces can only be removed if the function directly returns an expression if the body has statements the braces are required and so is the return Keyword
+(a) => a + 100;
+
+// 3. Remove the parameter parantheses
+// The parathesis  can only be removed if the function has a single simple parameter. the paranthesis remain if it has multiple parameters, no parameters or default, destructured or rest parameters
+(a) => a + 100;
