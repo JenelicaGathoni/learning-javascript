8e5e2aff602143c9fb3fa8468159b5c2a0c3dcb4
JenelicaGathoni <168233892+JenelicaGathoni@users.noreply.github.com>
2025-01-20 19:46:25 +0300

    styling the select form

---
+++ MDN/loops.js
@@ -0,0 +1,7 @@
+// loops offer a quick and easy way to do something repeatedly\
+// Telling someone to go five steps to the East
+for (let step = 0; step < 5; step++) {
+  console.log("Walking East one step");
+}
+// for loops
+//  for(initialisation; condition; afterthought ) statement
