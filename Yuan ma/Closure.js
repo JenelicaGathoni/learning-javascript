e1436bfa9ae3e343bdcf3298c2c3263c9c017a51
JenelicaGathoni <168233892+JenelicaGathoni@users.noreply.github.com>
2025-02-04 07:13:04 +0300

    Moved the files to different folders for Easy access and more organization

--- Yuan ma/Closure.js
+++
@@ -1,9 +0,0 @@
-//being able to reference a specific instance of a local variable in an enclosing scope;
-function wrapValue(n) {
-  let local = n;
-  return () => local;
-}
-let wrap1 = wrapValue(1);
-let wrap2 = wrapValue(2);
-console.log(wrap1(1));
-console.log(wrap2());
