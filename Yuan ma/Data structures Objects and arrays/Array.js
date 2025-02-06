2de211a19c930c9369e1b6ccab99af802764d075
JenelicaGathoni <168233892+JenelicaGathoni@users.noreply.github.com>
2025-02-06 15:38:51 +0300

    Learnt aabout arrays

---
+++ Yuan ma/Data structures Objects and arrays/Array.js
@@ -0,0 +1,8 @@
+let numbers = [2, 3, 5, 6, 11];
+console.log(numbers[2]);
+// there are two main ways to access values in javascript
+// Using square brackets and dot notation
+// when using a dot the word after the dot is the literal name of the property.
+// when using square brackets the expression between the brackets is evaluated to get the property name.
+// the dot notation only works with names that look like a valid variable name.
+//
