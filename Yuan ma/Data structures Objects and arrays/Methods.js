b59010ed06e835ffe7d2bb89d3b6759127709b9b
JenelicaGathoni <168233892+JenelicaGathoni@users.noreply.github.com>
2025-02-06 15:39:39 +0300

    Learnt about the various methods used in javascript.

---
+++ Yuan ma/Data structures Objects and arrays/Methods.js
@@ -0,0 +1,13 @@
+// toUpperCase
+let doh = "doh";
+console.log(doh.toUpperCase());
+// toLowerCase
+console.log(doh.toLowerCase());
+//push- adds values to the end of the array
+let numbers = [1, 3, 4, 5, 6, 6, 8];
+numbers.push(20, 50);
+numbers.push(56);
+console.log(numbers);
+// pop _ removes the last value in the array and returns it
+console.log(numbers.pop());
+console.log(numbers);
