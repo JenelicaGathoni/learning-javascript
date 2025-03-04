a1b9f02c2c49b690d0d269a393853f0813a2d2ab
JenelicaGathoni <168233892+JenelicaGathoni@users.noreply.github.com>
2025-03-05 02:19:00 +0300

    Tried implementing some Java script to  a html page

---
+++ BroCode/index.js
@@ -0,0 +1,3 @@
+document.getElementById("myH1").textContent = "this is a heading";
+document.getElementById("myP").textContent = "this is a paragraph";
+window.alert("This is a webpage created using Javascript");
