3a001cea8c3bb68dc4865664617f79377992772e
JenelicaGathoni <168233892+JenelicaGathoni@users.noreply.github.com>
2025-04-24 12:24:26 +0300

    Moved these files  into a different folder

--- BroCode/index.js
+++
@@ -1,6 +0,0 @@
-let jina = "Jenelica";
-let age = 20;
-let isStudent = false;
-document.getElementById("p1").textContent = `Name: ${jina}`;
-document.getElementById("p2").textContent = `Age: ${age}`;
-document.getElementById("p3").textContent = `Student: ${false}`;
