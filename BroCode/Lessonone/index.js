3e7fd3948141d7fb931139d80db0645e3dbcdc15
JenelicaGathoni <168233892+JenelicaGathoni@users.noreply.github.com>
2025-04-24 12:26:16 +0300

    Learned how to change the text content of elements using javascript

---
+++ BroCode/Lessonone/index.js
@@ -0,0 +1,6 @@
+let jina = "Jenelica";
+let age = 20;
+let isStudent = false;
+document.getElementById("p1").textContent = `Name: ${jina}`;
+document.getElementById("p2").textContent = `Age: ${age}`;
+document.getElementById("p3").textContent = `Student: ${false}`;
