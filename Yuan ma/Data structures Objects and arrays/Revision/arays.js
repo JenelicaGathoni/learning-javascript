83206a62593ddf507e2a69c6ed43d1ba4aa0e4d9
JenelicaGathoni <168233892+JenelicaGathoni@users.noreply.github.com>
2025-04-30 03:27:13 +0300

    Learned about arrays in Javascript

--- Yuan ma/Data structures Objects and arrays/Revision/arays.js
+++ Yuan ma/Data structures Objects and arrays/Revision/arays.js
@@ -30,3 +30,54 @@ console.log(myFavoriteSongs);
 console.log(myFavoriteSongs.length);
 myFavoriteSongs.pop();
 console.log(myFavoriteSongs);
+
+//shift and unshift methods
+
+let toDoList = ["maize", "milk", "potatoes"];
+//adding a task to the end of the array
+function remember(task) {
+  toDoList.push(task);
+}
+//removing the first item from the array
+function getTask() {
+  return toDoList.shift();
+}
+//adding a task to the front of the array
+function rememberUrgently(task) {
+  toDoList.unshift(task);
+}
+remember("cooking");
+console.log(toDoList);
+getTask();
+console.log(toDoList);
+rememberUrgently("cooking");
+console.log(toDoList);
+
+//To search for a specific value, arrays provide an indexOf method.
+//The method searches through the array from the start to the end
+// and returns the index at which the requested value was found—or -1 if it wasn’t found.
+let lostDreams = [
+  "Money",
+  "cars",
+  "Luxury",
+  "poshBuildings",
+  "cars",
+  "Expensiveclothes",
+  "Luxury",
+];
+console.log(lostDreams.indexOf("Luxury"));
+console.log(lostDreams.indexOf("Pajamas"));
+//To search from the end instead of the start, there’s a similar method called lastIndexOf
+console.log(lostDreams.lastIndexOf("Luxury"));
+//slice takes start and end indices
+//  and returns an array that has only the elements between them.
+//  The start index is inclusive and the end index is exclusive.
+//When the end index is not given, slice will take all of the elements after the start index.
+// You can also omit the start index to copy the entire array.
+console.log(lostDreams.slice(1, 5));
+console.log(lostDreams.slice(3));
+console.log(lostDreams.slice());
+//The concat method can be used to append arrays together to create a new array,
+//similar to what the + operator does for strings.
+let longArray = lostDreams.concat(myFavoriteFoods);
+console.log(longArray);
