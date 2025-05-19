f854a0b477e7fa37fe273dc35fc27a5f3dcd64a6
JenelicaGathoni <168233892+JenelicaGathoni@users.noreply.github.com>
2025-05-19 09:57:41 +0300

    Tried to learn about correlation ( I'll need to come back to this later)

---
+++ Yuan ma/Data structures Objects and arrays/Revision/correlation.js
@@ -0,0 +1,10 @@
+/*
+
+Correlation is a measure of dependence between statistical variables.
+Correlation between variables is usually expressed as a value that ranges from -1 to 1. 
+Zero correlation means the variables are not related.
+A correlation of 1 indicates that the two are perfectly related—if you know one, you also know the other.
+Negative 1 also means that the variables are perfectly related but are opposites—when one is true, the other is false.
+
+
+*/
