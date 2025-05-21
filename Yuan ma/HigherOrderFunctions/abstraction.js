d90e530a9624d9a1917de8969157ef504c8f9718
JenelicaGathoni <168233892+JenelicaGathoni@users.noreply.github.com>
2025-05-21 12:19:42 +0300

    Started learning about abstraction in Js

--- Yuan ma/HigherOrderFunctions/abstraction.js
+++ Yuan ma/HigherOrderFunctions/abstraction.js
@@ -1,3 +1,7 @@
+/*
+ Abstractions give us the ability to talk about problems at a higher (or more abstract) level,
+ without getting sidetracked by uninteresting details.
+ */
 let total = 0;
 let count = 1;
 while (count <= 11) {
