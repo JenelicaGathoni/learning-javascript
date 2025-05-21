bb633f6ab4ac33713ea3b050258de027a63f81d2
JenelicaGathoni <168233892+JenelicaGathoni@users.noreply.github.com>
2025-05-21 12:20:13 +0300

    did my third challenge

---
+++ General Practice/day3.js
@@ -0,0 +1,8 @@
+// Anonymous functions
+//An anonymous function is a function without a name.
+
+const students = [
+  { jina: "alice", age: 12 },
+  { jina: "Gathoni", age: 20 },
+  { jina: "Miriaa", age: 26 },
+];
