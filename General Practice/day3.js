7309809b4e692374246efc89614aca9b7c5731ff
JenelicaGathoni <168233892+JenelicaGathoni@users.noreply.github.com>
2025-05-25 11:45:52 +0300

    Practiced anonymous functions and the sort method

--- General Practice/day3.js
+++ General Practice/day3.js
@@ -1,8 +1,15 @@
 // Anonymous functions
 //An anonymous function is a function without a name.
-
+// We use anonymous functions because they:
+// short and direct
+//the logic lives right where it is used
+//Naming the function would only clutter the code
 const students = [
   { jina: "alice", age: 12 },
   { jina: "Gathoni", age: 20 },
   { jina: "Miriaa", age: 26 },
 ];
+students.sort(function (a, b) {
+  return b.age - a.age;
+});
+console.log(students);
