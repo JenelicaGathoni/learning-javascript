9789ce4ab86afae656b6f4f96f16410f6e9832e6
JenelicaGathoni <168233892+JenelicaGathoni@users.noreply.github.com>
2025-06-17 05:57:00 +0300

    Learned about the map method in js

--- Yuan ma/HigherOrderFunctions/map.js
+++ Yuan ma/HigherOrderFunctions/map.js
@@ -1,5 +1,22 @@
+/**
+ * Map is like a factory:
+ * You give it a bunch of items
+ * It performs a transformation
+ * It returns the transformed items
+ */
 const fruits = ["Mango", "Orange", "Banana", "Kiwi"];
 const juices = fruits.map(function (fruit) {
   return fruit + " Juice";
 });
 console.log(juices);
+const brands = ["Jordan", "Nike", "Addidas"];
+const shoes = brands.map(function (brand) {
+  return brand + " shoes";
+});
+console.log(shoes);
+
+const clothes = ["dress", "hat", "shirt", "underwear"];
+const glittery = clothes.map(function (clothe) {
+  return "glittery" + " " + clothe;
+});
+console.log(glittery);
