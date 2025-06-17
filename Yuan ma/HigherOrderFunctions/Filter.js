52f61f369c78268f92e280bc7fdaf391213b21a4
JenelicaGathoni <168233892+JenelicaGathoni@users.noreply.github.com>
2025-06-17 05:56:06 +0300

    Learned about the filter method in Javascript

--- Yuan ma/HigherOrderFunctions/Filter.js
+++ Yuan ma/HigherOrderFunctions/Filter.js
@@ -1,3 +1,8 @@
+/**
+ *Filter is like a magnet
+ *You give a bunch of things
+ *It only picks the things with the specific quality you want(In this case magnetism)
+ */
 // Using filter method to obtain an array of only the fresh tomatoes in a kitchen
 const tomatoes = [
   { Name: "Refu", isFresh: false },
@@ -9,3 +14,9 @@ const freshTomatoes = tomatoes.filter(function (tomato) {
   return tomato.isFresh;
 });
 console.log(freshTomatoes);
+
+let ingredients = ["Carrot", "onion", "bone", "rock", "brocolli"];
+let edible = ingredients.filter(function (item) {
+  return (item !== "bone") & (item !== "rock");
+});
+console.log(edible);
